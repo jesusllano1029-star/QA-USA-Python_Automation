814d4c1eac0d2fa2a8f1d3eb017c57401dba67aa
jesus <@jesusllano1029-star>
2025-08-20 22:11:41 -0500

    Project 7

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
-# File with constants
+URBAN_ROUTES_URL='https://cnt-c1dd512b-b9bc-4c42-a6ee-86e63a13d24b.containerhub.tripleten-services.com/'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
