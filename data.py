cb0b8392911331faba0f199479adf6ba034499ac
jesus <@jesusllano1029-star>
2025-09-19 16:08:35 -0500

    Project 8

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL='https://cnt-b4ff6262-d02b-4d0a-9ca7-2d2e59d54747.containerhub.tripleten-services.com/'
+URBAN_ROUTES_URL='https://cnt-6041bdaf-6220-4bba-882e-c026374971f8.containerhub.tripleten-services.com/'
 
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
--- pages.py
+++ pages.py
@@ -1,12 +1,11 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import (
     ElementClickInterceptedException,
     ElementNotInteractableException,
     TimeoutException,
 )
-import data
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class UrbanRoutesPage:
